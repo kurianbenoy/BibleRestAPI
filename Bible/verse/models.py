5d02dd8117d01dd879482ba8e88cd8d0bbac4003
kurianbenoy <kurian.pro@gmail.com>
2018-07-31 14:54:49 +0530

    Add models for verse

--- Bible/verse/models.py
+++ Bible/verse/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Bible(models.Model):
+    book = models.IntegerField(db_column='Book', blank=True, null=True)  # Field name made lowercase.
+    chapter = models.IntegerField(db_column='Chapter', blank=True, null=True)  # Field name made lowercase.
+    versecount = models.IntegerField(db_column='Versecount', blank=True, null=True)  # Field name made lowercase.
+    verse = models.CharField(max_length=1024, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'BIBle'
