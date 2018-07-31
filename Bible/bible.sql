52d6c804e304db185dfc18881ce266cfed800e60
kurianbenoy <kurian.pro@gmail.com>
2018-07-31 14:48:39 +0530

    Changed Django app layout

---
+++ Bible/bible.sql
@@ -0,0 +1,3 @@
+CREATE TABLE BIBle (Book int(2), Chapter INTEGER, Versecount INTEGER, verse varchar(1024));
+INSERT INTO BIBle (Book,Chapter,Versecount,verse)
+VALUES(0,1,1,'ആദിയിൽ ദൈവം ആകാശവും ഭൂമിയും സൃഷ്ടിച്ചു.');
--- bible/randomverse/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RandomverseConfig(AppConfig):
-    name = 'randomverse'
